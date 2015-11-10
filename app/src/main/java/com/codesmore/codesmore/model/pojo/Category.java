42669912dd87d2ea04ecf3796fe0f1374714738b
Vesko <iliev.vesselin@gmail.com>
2015-11-10 06:24:12 +0200

    Add helper methods to the POJOs

--- app/src/main/java/com/codesmore/codesmore/model/pojo/Category.java
+++ app/src/main/java/com/codesmore/codesmore/model/pojo/Category.java
@@ -1,9 +1,16 @@
 package com.codesmore.codesmore.model.pojo;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+
 /**
  * Created by demouser on 11/9/15.
  */
 public class Category {
+
+    public static Category from(Cursor cursor) {
+        throw new RuntimeException("Implement me!!!");
+    }
 
     private String name;
     private String imageUrl;
@@ -11,6 +18,10 @@ public class Category {
     public Category(String name, String imageUrl) {
         this.name = name;
         this.imageUrl = imageUrl;
+    }
+
+    public ContentValues toContentValues() {
+        throw new RuntimeException("Implement me!!!");
     }
 
     public String getName() {
--- app/src/main/java/com/codesmore/codesmore/model/pojo/Report.java
+++ app/src/main/java/com/codesmore/codesmore/model/pojo/Report.java
@@ -1,9 +1,16 @@
 package com.codesmore.codesmore.model.pojo;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+
 /**
  * Created by demouser on 11/9/15.
  */
 public class Report {
+
+    public static Report from(Cursor cursor) {
+        throw new RuntimeException("Implement me!!!");
+    }
 
     private Category category;
     private String description;
@@ -13,6 +20,10 @@ public class Report {
         this.description = description;
     }
 
+    public ContentValues toContentValues() {
+        throw new RuntimeException("Implement me!!!");
+    }
+    
     public Category getCategory() {
         return category;
     }
